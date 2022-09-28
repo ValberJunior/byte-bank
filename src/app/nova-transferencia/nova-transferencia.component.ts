cf2b64fb487a43dabc870e37f29bedb9a348720f
ValberJunior <valberjr1992@gmail.com>
2022-09-27 23:35:27 -0300

    create nova-transferencia component

---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,41 @@
+import { Component, EventEmitter, Output } from "@angular/core";
+
+//Decorator
+@Component({
+    selector:'app-nova-transferencia',
+    templateUrl: './nova-transferencia.component.html',
+    styleUrls: ['./nova-transferencia.component.scss']
+})
+
+export class NovaTransferenciaComponent {
+  //metadado
+  @Output() whenTransferring = new EventEmitter<any>();
+  @Output() transferError = new EventEmitter<string>();
+
+  //propriedades
+  value: number = 0;
+  destination: number = 0;
+
+  //metodos
+  transferValue(){
+      if(this.itsValid()){
+        const emitValue = {
+          value: this.value, destination: this.destination
+         };
+       this.whenTransferring.emit(emitValue);
+       this.transferError.emit("Transferência realizada com sucesso!")
+       this.clearFields();
+      }
+  };
+
+  clearFields(){
+    this.value = 0;
+    this.destination = 0;
+  }
+
+  private itsValid(){
+    const valid = this.value > 0 ;
+    if (!valid) return this.transferError.emit("Insira um valor válido");
+    return valid;
+  }
+}
