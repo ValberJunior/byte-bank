d4ff8be04fe2487a288ee1d48e441c92d25cf9a4
ValberJunior <valberjr1992@gmail.com>
2022-09-27 23:35:09 -0300

    update component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { ITransactionParams } from './../interfaces/ITransactionParams';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  //propriedades
   title = 'byte-bank';
+  transaction : ITransactionParams = {
+    value: 0,
+    destination: 0
+  };
+  displayMessage : string =  ""
+
+  //metodos
+  transfer($event:any) {
+    console.log($event);
+    this.transaction = $event;
+  }
+
+  displayStatus($event: string){
+    this.displayMessage = $event;
+  }
 }
